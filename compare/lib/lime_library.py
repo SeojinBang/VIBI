d775e96783c2d9ff7969fe432a39222d904ccdd2
Seojin Bang <seojinb@cs.cmu.edu>
2019-01-06 21:04:34 -0500

    Update lime library for text

--- compare/lib/lime_library.py
+++ compare/lib/lime_library.py
@@ -31,6 +31,19 @@ from lime.wrappers.scikit_image import SegmentationAlgorithm
 from sklearn.linear_model import Ridge, lars_path, LogisticRegression
 #%%
 
+class TextExplanationModified(object):
+    def __init__(self, test, segments):
+        """Init function.
+        Args:
+            text: text
+            segments: 2d numpy array, with the output from skimage.segmentation
+        """
+        self.text = text
+        self.segments = segments
+        self.intercept = {}
+        self.local_exp = {}
+        self.local_pred = None
+        
 class ImageExplanationModified(object):
     def __init__(self, image, segments):
         """Init function.
@@ -92,6 +105,195 @@ class ImageExplanationModified(object):
                         continue
                     # temp[segments == f, cp] *= 0.5
             return temp, mask
+#%%
+class LimeTextExplainerModified(object):
+
+    def __init__(self, kernel_width=.25, kernel=None, verbose=False,
+                 feature_selection='auto', random_state=None, is_cuda = False, dataset = None):
+        """Init function.
+        Args:
+            kernel_width: kernel width for the exponential kernel.
+            If None, defaults to sqrt(number of columns) * 0.75.
+            kernel: similarity kernel that takes euclidean distances and kernel
+                width as input and outputs weights in (0,1). If None, defaults to
+                an exponential kernel.
+            verbose: if true, print local prediction values from linear model
+            feature_selection: feature selection method. can be
+                'forward_selection', 'lasso_path', 'none' or 'auto'.
+                See function 'explain_instance_with_data' in lime_base.py for
+                details on what each of the options does.
+            random_state: an integer or numpy.RandomState that will be used to
+                generate random numbers. If None, the random state will be
+                initialized using the internal numpy seed.
+        """
+        kernel_width = float(kernel_width)
+
+        if kernel is None:
+            def kernel(d, kernel_width):
+                return np.sqrt(np.exp(-(d ** 2) / kernel_width ** 2))
+
+        kernel_fn = partial(kernel, kernel_width=kernel_width)
+
+        self.random_state = check_random_state(random_state)
+        self.feature_selection = feature_selection
+        self.base = LimeBaseModified(kernel_fn, verbose, random_state=self.random_state)
+        self.is_cuda = is_cuda
+        self.dataset = dataset
+        
+    def explain_instance(self, text, filter_size, classifier_fn, labels=(1,),
+                         hide_color=0,
+                         top_labels=5, num_features=100000, num_samples=1000,
+                         batch_size=10,
+                         segmentation_fn=None,
+                         segments=None,
+                         distance_metric='cosine',
+                         model_regressor=None,
+                         random_seed=None):
+        """Generates explanations for a prediction.
+        First, we generate neighborhood data by randomly perturbing features
+        from the instance (see __data_inverse). We then learn locally weighted
+        linear models on this neighborhood data to explain each of the classes
+        in an interpretable way (see lime_base.py).
+        Args:
+            text: text
+            classifier_fn: classifier prediction probability function, which
+                takes a numpy array and outputs prediction probabilities.  For
+                ScikitClassifiers , this is classifier.predict_proba.
+            labels: iterable with labels to be explained.
+            hide_color: TODO
+            top_labels: if not None, ignore labels and produce explanations for
+                the K labels with highest prediction probabilities, where K is
+                this parameter.
+            num_features: maximum number of features present in explanation
+            num_samples: size of the neighborhood to learn the linear model
+            batch_size: TODO
+            distance_metric: the distance metric to use for weights.
+            model_regressor: sklearn regressor to use in explanation. Defaults
+            to Ridge regression in LimeBase. Must have model_regressor.coef_
+            and 'sample_weight' as a parameter to model_regressor.fit()
+            segmentation_fn: SegmentationAlgorithm, wrapped skimage
+            segmentation function
+            random_seed: integer used as random seed for the segmentation
+                algorithm. If None, a random integer, between 0 and 1000,
+                will be generated using the internal random number generator.
+        Returns:
+            An Explanation object (see explanation.py) with the corresponding
+            explanations.
+           
+        """
+        if random_seed is None:
+            random_seed = self.random_state.randint(0, high=1000)
+
+        if segments is None:
+
+            if segmentation_fn is None:
+                segmentation_fn = SegmentationAlgorithm('quickshift', kernel_size=4,
+                                                    max_dist=200, ratio=0.2,
+                                                    random_seed=random_seed)
+            try:
+                segments = segmentation_fn(text) # (1, 28, 28)
+
+            except ValueError as e:
+                raise e
+        
+        print(text.size())
+        chunked_text = copy.deepcopy(text)
+        chunked_text = chunked_text.squeeze(0)
+        chunked_text = chunked_text.cpu().numpy()
+        print(chunked_text.shape)
+        
+        if filter_size[0] * filter_size[1] > 1:
+            print(segments.shape)# (1, 15, 50)
+            print(chunked_text.shape) # 750 * 100
+            print(np.mean(chunked_text[segments ==1]))
+            for x in np.unique(segments):
+                chunked_text[segments == x] = np.mean(chunked_text[segments == x])
+   
+        fudged_text = copy.deepcopy(chunked_text)
+        fudged_text[:] = hide_color
+        
+        top = labels
+
+        #print(torch.Tensor(chunked_text).unsqueeze(0).size())
+        data, labels, neighborhood_data = self.data_labels(#text,
+            torch.Tensor(chunked_text).unsqueeze(0),
+                                        fudged_text, segments,
+                                        classifier_fn, num_samples,
+                                        batch_size=batch_size)
+
+        if filter_size[0] * filter_size[1] > 1:
+            neighborhood_data = F.avg_pool2d(torch.Tensor(neighborhood_data).view(num_samples, segments.shape[-2], segments.shape[-1]), kernel_size = filter_size, stride = filter_size, padding = 0)
+            neighborhood_data = neighborhood_data.cpu().numpy().reshape(num_samples, neighborhood_data.size(-2) * neighborhood_data.size(-1))
+            
+        distances = sklearn.metrics.pairwise_distances(
+            data,
+            data[0].reshape(1, -1),
+            metric=distance_metric
+        ).ravel()
+
+        ret_exp = TextExplanationModified(chunked_text, segments)
+       
+        if top_labels:
+            top = np.argsort(labels[0])[-top_labels:]
+            ret_exp.top_labels = list(top)
+            ret_exp.top_labels.reverse()
+
+        t3 = time.time()
+        for label in ret_exp.top_labels:
+            (ret_exp.intercept[label],
+             ret_exp.local_exp[label],
+             ret_exp_local_pred_proba, ret_exp_local_pred) = self.base.explain_instance_with_data(
+                neighborhood_data, labels,
+                distances, label, num_features,
+                model_regressor = model_regressor,
+                feature_selection = self.feature_selection)
+                
+            if label == ret_exp.top_labels[0]:
+                ret_exp.local_pred_proba = [ret_exp_local_pred_proba]
+                ret_exp.local_pred = [ret_exp_local_pred]
+            else:
+                #print(ret_exp_score, ret_exp_local_pred)
+                ret_exp.local_pred_proba.append(ret_exp_local_pred_proba)
+                ret_exp.local_pred.append(ret_exp_local_pred)
+
+        t4 = time.time()
+        ret_exp.local_pred_proba = np.array(ret_exp.local_pred_proba)[np.argsort(ret_exp.top_labels)]
+
+        return ret_exp
+    
+    def data_labels(self,
+                    text,
+                    fudged_text,
+                    segments,
+                    classifier_fn,
+                    num_samples,
+                    batch_size=10):
+
+        n_features = np.unique(segments).shape[0]
+        data = self.random_state.randint(0, 2, num_samples * n_features).reshape((num_samples, n_features))
+        #labels = []
+        data[0, :] = 1
+        imgs = []
+        for row in data:
+            temp = copy.deepcopy(text.squeeze(0)).cpu().numpy()
+            zeros = np.where(row == 0)[0]
+            mask = np.zeros(segments.shape).astype(bool)
+            for z in zeros:
+                mask[segments == z] = True
+            temp[mask] = fudged_text[mask]
+            #print(temp.shape)
+            imgs.append(temp)
+        
+        preds = classifier_fn(cuda(torch.Tensor(imgs), self.is_cuda)).detach().cpu().numpy()
+        if self.dataset == 'mnist':
+            neighborhood_data = np.reshape(np.squeeze(np.stack(imgs, axis=0), axis=1), (num_samples, -1))
+        elif self.dataset == 'imdb':
+            neighborhood_data = np.mean(np.stack(imgs, axis=0), axis = -1)
+        else:
+            raise ValueError('unknown dataset')
+            
+        return data, np.array(preds), neighborhood_data
+   
 
 #%%
 class LimeImageExplainerModified(object):
@@ -200,12 +402,6 @@ class LimeImageExplainerModified(object):
         if filter_size[0] * filter_size[1] > 1:
             #image = F.avg_pool2d(image, kernel_size = filter_size, stride = filter_size, padding = 0)
             #image = F.max_unpool2d(image, kernel_size = filter_size, stride = filter_size, padding = 0)
-
-            print('here')
-            #print(segments)
-            print(segments.shape)# (1, 15, 50)
-            print(chunked_image.shape) # 750 * 100
-            print(np.mean(chunked_image[segments ==1]))
             for x in np.unique(segments):
                 chunked_image[segments == x] = np.mean(chunked_image[segments == x])
             #chunked_image = torch.Tensor(chunked_image).unsqueeze(0)
