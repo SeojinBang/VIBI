cdd6f6e0a377451cd566b6c3ecb6166eabadde85
SeojinBang <seojinb@cs.cmu.edu>
2019-01-17 02:46:29 -0800

    sample summary function

--- imdb/summary_text.py
+++ imdb/summary_text.py
@@ -14,11 +14,14 @@ def main(args):
     
     filename = args.filename
     result_dir = args.result_dir
-
+    output_dir = args.output_dir
+    
     print('summarize {} and save in {}'.format(filename, result_dir))
     
-    fns = np.array([filename + '_idx0.txt', filename + '_idx200.txt'])
-
+#    fns = np.array([filename + '_idx0.txt', filename + '_idx200.txt'])
+    idx_list = [0, 1, 2, 4, 120, 121, 123, 124, 197, 198, 199, 200]
+    fns = np.array([filename + '_idx' + str(item) + '.txt' for item in idx_list])
+    
     contents_all = []
     sent = []
     pred = []
@@ -28,24 +31,33 @@ def main(args):
         f = open(result_dir + '/' + fn, 'r')
         if f.mode == 'r':
             contents = f.read()
+            contents = re.sub(' br ', '...<br />...', contents)
+            contents = re.sub('<unk>', '...', contents)
             contents = re.sub('_+', ' ', contents).strip()
-            contents = re.sub('\s{2,}', ', ', contents)
+            contents = re.sub('\s{2,}', '...<br />... ', contents)
             contents = contents.split("sent:")
             contents = list(filter(None, contents))
             contents = ["sent:" + x.strip() for x in contents]
-            contents = [item[(item.find('approx:')+10):] for item in contents]
-            contents = [re.sub(r'^,|,$', "", item).strip() for item in contents]
 
-            contents_all = contents_all.extend(contents)
-            sent = sent.extend([item[(item.find('sent:')+5):(item.find('sent:')+8)] for item in contents])
-            pred = pred.extend([item[(item.find('pred:')+5):(item.find('pred:')+8)] for item in contents])
-            approx = approx.extend([item[(item.find('approx:')+7):(item.find('approx:')+10)] for item in contents])            
-            
-    mydata = [contents_all, sent, pred, approx]
-    myfile = open(filename + '.csv', 'w')
+            sent.extend([item[(item.find('sent:')+5):(item.find('sent:')+8)] for item in contents])
+            pred.extend([item[(item.find('pred:')+5):(item.find('pred:')+8)] for item in contents])
+            approx.extend([item[(item.find('approx:')+7):(item.find('approx:')+10)] for item in contents])            
+
+            contents = [item[(item.find('approx:')+10):] for item in contents]
+            contents = [re.sub(r'^<br />|<br />$', '', item.strip()).strip() for item in contents]
+
+            contents_all.extend(contents)
+        f.close()
+
+    
+    mydata = zip(contents_all, sent, pred, approx)
+    myfile = open(output_dir + '/' + filename + '.csv', 'w')
     with myfile:
         writer = csv.writer(myfile)
-        writer.writerows(mydata)
+        writer.writerow(('keys', 'sent', 'pred', 'approx'))
+        for row in mydata:
+            if row[0] != '' and row[0] is not None:
+                writer.writerow(row)
 
     print(mydata)
 
@@ -55,7 +67,7 @@ if __name__ == "__main__":
     
     parser.add_argument('--filename', type = str, help='File name')
     parser.add_argument('--result_dir', type = str, help='Result directory path')
-
+    parser.add_argument('--output_dir', default = '.', type = str, help='save output in')
     args = parser.parse_args()
     
     main(args)
